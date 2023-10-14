e59dbfad9cff1048c6c085dbfbf6894b17e0eea1
sally <119037864+ht18boynd@users.noreply.github.com>
2023-10-14 12:13:30 +0700

    Constructor

---
+++ EZD_API/src/main/java/com/ezd/EzdApiApplication.java
@@ -0,0 +1,13 @@
+package com.ezd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EzdApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EzdApiApplication.class, args);
+	}
+
+}
---
+++ EZD_API/src/test/java/com/ezd/EzdApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ezd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EzdApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
