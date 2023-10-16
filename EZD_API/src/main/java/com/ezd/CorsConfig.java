c961246723c0089ec2c5cd3a2134268eaecfe9e4
sally <119037864+ht18boynd@users.noreply.github.com>
2023-10-16 07:04:13 +0700

    crosconfig

---
+++ EZD_API/src/main/java/com/ezd/CorsConfig.java
@@ -0,0 +1,28 @@
+package com.ezd;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("http://localhost:3000/"); // Đặt origin của ứng dụng React của bạn ở đây
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("OPTIONS");
+        config.addAllowedMethod("GET");
+        config.addAllowedMethod("POST");
+        config.addAllowedMethod("PUT");
+        config.addAllowedMethod("DELETE");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+   
+  
+}
--- EZD_API/src/main/java/com/ezd/controllers/GameController.java
+++ EZD_API/src/main/java/com/ezd/controllers/GameController.java
@@ -4,6 +4,7 @@ package com.ezd.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,8 @@ import com.ezd.service.GameService;
 
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000/")
+
 @RequestMapping("/api/games") // Điều chỉnh đường dẫn cơ sở cho API
 public class GameController {
 
@@ -34,18 +37,18 @@ public class GameController {
         return gameService.getGameById(id);
     }
 
-    @PostMapping
+    @PostMapping("/add")
     public Game addGame(@RequestBody Game game) {
         return gameService.save(game);
     }
 
-    @PutMapping("/{id}")
+    @PutMapping("/edit/{id}")
     public Game updateGame(@PathVariable int id, @RequestBody Game game) {
         game.setIdGame(id); // Đảm bảo rằng id của Game không thay đổi
         return gameService.save(game);
     }
 
-    @DeleteMapping("/{id}")
+    @DeleteMapping("/delete/{id}")
     public void deleteGame(@PathVariable int id) {
         gameService.delete(id);
     }
