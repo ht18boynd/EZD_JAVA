48725e6f7e889a3e57fbd6d31b8a5396c94d5b2d
sally <119037864+ht18boynd@users.noreply.github.com>
2023-10-20 21:09:53 +0700

    v-20-10-2

--- EZD_API/src/main/java/com/ezd/EzdApiApplication.java
+++ EZD_API/src/main/java/com/ezd/EzdApiApplication.java
@@ -2,6 +2,8 @@ package com.ezd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.multipart.MultipartResolver;
 
 @SpringBootApplication
 public class EzdApiApplication {
@@ -9,5 +11,7 @@ public class EzdApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EzdApiApplication.class, args);
 	}
+	
+
 
 }
--- EZD_API/src/main/java/com/ezd/controllers/PerfectRoleController.java
+++ EZD_API/src/main/java/com/ezd/controllers/PerfectRoleController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ezd.models.PerfectRole;
@@ -34,18 +35,24 @@ public class PerfectRoleController {
 	    }
 
 	    @PostMapping("/add")
-	    public PerfectRole createPerfectRole(@RequestBody PerfectRole role) {
+	    public PerfectRole createPerfectRole(@RequestParam("name") String roleName) {
+	        PerfectRole role = new PerfectRole();
+	        role.setName(roleName);
 	        return repository.save(role);
 	    }
 
+
 	    @PutMapping("/edit/{id}")
-	    public PerfectRole updatePerfectRole(@PathVariable Long id, @RequestBody PerfectRole role) {
-	        if (repository.existsById(id)) {
-	        	role.setId(id);
+	    public PerfectRole updatePerfectRole(@PathVariable Long id, @RequestParam("name") String name) {
+	        Optional<PerfectRole> existingRole = repository.findById(id);
+	        if (existingRole.isPresent()) {
+	            PerfectRole role = existingRole.get();
+	            role.setName(name);
 	            return repository.save(role);
 	        }
 	        return null;
 	    }
+
 
 	    @DeleteMapping("/delete/{id}")
 	    public void deletePerfectRole(@PathVariable Long id) {
