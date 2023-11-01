38cea5f82d1218c352bc2be0cedaf3a58455ea42
sally <119037864+ht18boynd@users.noreply.github.com>
2023-11-01 11:50:01 +0700

    v01-11 v1

--- EZD_API/src/main/java/com/ezd/controllers/AuthenticationController.java
+++ EZD_API/src/main/java/com/ezd/controllers/AuthenticationController.java
@@ -6,18 +6,30 @@ import com.ezd.Dto.RefreshTokenRequest;
 import com.ezd.Dto.SignInRequest;
 import com.ezd.Dto.SignUpRequest;
 import com.ezd.models.Auth;
+import com.ezd.models.Game;
+import com.ezd.repository.AuthRepository;
 import com.ezd.service.AuthenticationService;
 import lombok.RequiredArgsConstructor;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
 public class AuthenticationController {
+	@Autowired 
+	private AuthRepository authRepository;
     private  final AuthenticationService authenticationService;
 
     @PostMapping("/signup")
@@ -30,8 +42,19 @@ public class AuthenticationController {
         return  ResponseEntity.ok(authenticationService.signin(signInRequest));
     }
 
-    @PostMapping("refresh")
+    @PostMapping("/refresh")
     public  ResponseEntity<JwtAuthenticationResponse> refresh(@RequestBody RefreshTokenRequest refreshTokenRequest) {
         return  ResponseEntity.ok(authenticationService.refreshToken(refreshTokenRequest));
     }
+    @GetMapping("/findByEmail")
+    public ResponseEntity<Optional<Auth>> findByEmail(@RequestParam String email) {
+        Optional<Auth> auth = authRepository.findByEmail(email);
+
+        if (auth != null) {
+            return ResponseEntity.ok(auth);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+  
 }
